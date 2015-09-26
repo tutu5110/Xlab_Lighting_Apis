fdcc38ff1ca0ccb15c166c2c8329239fcd0ce2b7
jing <jqian1590@gmail.com>
2015-09-26 15:55:23 -0400

    update on fadeIn function

--- LED_APIs/LED_APIs.ino
+++ LED_APIs/LED_APIs.ino
@@ -26,42 +26,112 @@ unsigned int a = 0;
 
 unsigned int quadrant;
 
+int  m_blue;
+int  m_red;
+int  m_green;
+
+
 void setup() {
 #if defined (__AVR_ATtiny85__)
   if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
 #endif  
-
+Serial.begin(9600);                
+m_blue = 0;
+m_red = 0;
+m_green = 0;
 pixels.begin(); // This initializes the NeoPixel library.
   
-  for(int i=0;i<NUMPIXELS;i++){
-    
-    pixels.setPixelColor(i, pixels.Color(0,15,0)); // Moderately bright green color.
-    
-    pixels.show(); // This sends the updated pixel color to the hardware.
-    
-    delay(delayval); // Delay for a period of time (in milliseconds).
-
-  }
+//  for(int i=0;i<NUMPIXELS;i++){
+//    
+//    pixels.setPixelColor(i, pixels.Color(0,15,0)); // Moderately bright green color.
+//    
+//    pixels.show(); // This sends the updated pixel color to the hardware.
+//    
+//    delay(delayval); // Delay for a period of time (in milliseconds).
+//
+//  }
+  fadeInColor(1,23,25,25,true,1);
+  
 }
 
 void loop() {
 
-  changeColor(1,23,25,25);
+ // changeColor(1,23,25,25);
+
+
 }
 
 
 /********* Single Panel Functions *************/
 
 void fadeInColor(int quad, int r, int g, int b){
-  
+   int _time = 5;
+  int _start = quad - 1;
+  if(_start < 0 ) _start = 0;
+  _start = _start * PXQUADS;
+  int _end = quad * PXQUADS;
+  //initialize
+  uint32_t color = pixels.getPixelColor(_start);
+  for(int i = _start; i < _end; i ++){
+     color = pixels.getPixelColor(i);
+      m_red = (color >> 16) & 0xff;
+      m_green = (color >> 8) & 0xff;
+      m_blue = color & 0xff;
+      m_red+=1;
+      m_green+=1;
+      m_blue+=1;
+      pixels.setPixelColor(i, pixels.Color(confine(m_red,r),confine(m_green,g),confine(m_blue,b)));
+       delay(_time);
+   }
+    pixels.show();
 }
 
-void fadeInColor(int quad, int r, int g, int b, boolean animated){
+boolean fadeInColor(int quad, int r, int g, int b, boolean animated, int index){
+  if(index == 0){
+    index = (quad -1) * PXQUADS;
+  }
+  int _time = 5;
+  int _start = quad - 1;
+  if(_start < 0 ) _start = 0;
+  _start = _start * PXQUADS;
+  int _end = quad * PXQUADS;
+   uint32_t colorCu = pixels.getPixelColor(_start);
+   //check last color
+      uint32_t color = pixels.getPixelColor(_end);
+     
+    //if sequence is completed
+  if(index > _end)
+      return true;
+
+     
+  for(int i = _start; i < index; i ++){
+       //get previous
+      color = pixels.getPixelColor(i);
+      m_red = (color >> 16) & 0xff;
+      m_green = (color >> 8) & 0xff;
+      m_blue = color & 0xff;  
+      m_red+=2;
+      m_green+=2;
+      m_blue+=2;
+      pixels.setPixelColor(i, pixels.Color(confine(m_red,r),confine(m_green,g),confine(m_blue,b)));
+       delay(_time);
+   }
+    pixels.show();
+    
+    if(m_red >= r)
+      index ++;
+      
+    fadeInColor(1,23,25,25,true,index);
+
 }
 
+
+// Change led color in a quad of the loop, without animation.
 void changeColor(int quad, int r, int g, int b){
   changeColor(quad,r,g,b,false);
 }
+
+// Change led color in a quad of the loop, with animation.
 void changeColor(int quad, int r, int g, int b, boolean animated){
   int _time = 10;
   int _start = quad - 1;
@@ -74,6 +144,13 @@ void changeColor(int quad, int r, int g, int b, boolean animated){
         pixels.show();
         delay(_time);
        }
-       pixels.show();
-  }
+   }
+    pixels.show();
+}
+
+// confines a range of numbers
+int confine(int num, int upperlimit){
+  if(num > upperlimit)
+    return upperlimit;
+   return num;
 }
